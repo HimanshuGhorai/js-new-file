92afd6832977c662c747a18ea37dbbdcefd355cf
Himanshu <140635492+HimanshuGhorai@users.noreply.github.com>
2024-11-21 17:39:00 +0000

    data types in js file

---
+++ js/01_basic/data.js
@@ -0,0 +1,27 @@
+// "use strict"
+
+// console.log("hello");
+
+// number = > 1234567890
+
+// bigint => user for big number 
+
+// stirng => "this is a string"
+
+// boolean => ture/fulse 
+
+// null => standalone value 
+
+// undefined => it is not defined yet 
+
+// symbal => unique 
+
+console.log(typeof undefined) // this is undefined 
+
+console.log(typeof null); // this is a object
+
+
+
+// alert("hello") this is not work reason we are using node js
+
+// console.log( typeof (nall))  
